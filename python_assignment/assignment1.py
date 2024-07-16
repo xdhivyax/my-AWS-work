48e032827000591d66a74c3050954da6243e22f4
Dhivya <113448671+xdhivyax@users.noreply.github.com>
2024-07-16 12:01:57 +0530

    Delete python_assignment directory

--- python_assignment/assignment1.py
+++
@@ -1,8 +0,0 @@
-a=int(input("Enter the a value :"))
-b=int(input("Enter the b value :"))
-if a>b:
-	print("a is greater")
-elif a==b :
-	print ("Both a and b are equal")
-else :
-	print("b is greater")
--- python_assignment/assignment2.py
+++
@@ -1,9 +0,0 @@
-a=int(input("Enter the a value :"))
-b=int(input("Enter the b value :"))
-c=int(input("Enter the c value :"))
-if (a>=b and a>=c):
-	print(f"{a} is greater")
-elif (b>=a and b>=c):
-    print(f"{b} is greater")
-else:
-	print(f"{c} is greater")
